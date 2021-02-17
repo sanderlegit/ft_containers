8ca21da6aa498ce45dc0496e1896262b38133ff9
MrOsteoporosis <sanderverheijen@outlook.com>
2021-02-17 16:49:14 +0100

    couple more list functions, started writing bi_iterator

--- list/list.hpp
+++ list/list.hpp
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/16 10:48:13 by averheij      #+#    #+#                 */
-/*   Updated: 2021/02/17 14:30:20 by averheij      ########   odam.nl         */
+/*   Updated: 2021/02/17 16:48:32 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,30 @@
 # include <iterator>
 # include <cstddef>
 
+
+#include <iostream>
+
 /*	memory		std::allocator
- *	iterator	bidirectional_iterator_tag
- *	cstddef		ptrdiff_t
-*/
+ *	iterator	bidirectional_iterator_tag //this doesnt look right
+ *	cstddef		ptrdiff_t */
 
 namespace ft {
 	template <class T, class Alloc = std::allocator<T> >
-	class list {
+	class list { //https://cplusplus.com/reference/list/list/
 
 		public:
 			/*member type				definition notes
 			 *value_type				The first template parameter (T)	
-			 *
 			 *allocator_type			The second template parameter (Alloc)	
-			 *
 			 *reference					allocator_type::reference				
-			 *
 			 *const_reference			allocator_type::const_reference			
-			 *
 			 *pointer					allocator_type::pointer					
-			 *
 			 *const_pointer				allocator_type::const_pointer			
-			 *
-			 *iterator					a bidirectional iterator to value_type	
-			 *
-			 *const_iterator			a bidirectional iterator to const value_type	
-			 *
-			 *reverse_iterator			reverse_iterator<iterator>	
-			 *
-			 *const_reverse_iterator	reverse_iterator<const_iterator>	
-			 *
 			 *difference_type			a signed integral type, identical to:	
 			 *							iterator_traits<iterator>::difference_type	
-			 *
 			 *size_type					an unsigned integral type that can 		
 			 *							represent any non-negative value of 
-			 *							difference_type	
-			*/
+			 *							difference_type	*/
 
 			typedef	T										value_type;
 			typedef	Alloc									allocator_type;
@@ -62,20 +48,14 @@ namespace ft {
 			typedef	const value_type&						const_reference;
 			typedef	value_type*								pointer;
 			typedef	const value_type*						const_pointer;
-			//TODO
-			//typedef	std::bidirectional_iterator_tag<>		iterator;
-			//typedef	std::bidirectional_iterator_tag<const>	const_iterator;
-			//typedef	std::reverse_iterator<iterator>			reverse_iterator;
-			//typedef	std::reverse_iterator<const_iterator>	const_reverse_iterator;
 			typedef	ptrdiff_t								difference_type;
 			typedef size_t									size_type;
 
 		private:
-
 			class link {
 				public:
 					link(void) : next(NULL), prev(NULL) {}
-					link(const T& data_, link *next_ = NULL, link* prev_ = NULL) : data(data_), next(next_), prev(prev_) {}
+					link(T* data_, link *next_ = NULL, link* prev_ = NULL) : data(data_), next(next_), prev(prev_) {}
 
 					~link(void) {}
 
@@ -88,20 +68,84 @@ namespace ft {
 						return *this;
 					}
 
-					T			data;
+					T			*data;
 					link		*next;
 					link		*prev;
 			};
 
-		private:
+		public:
+			typedef link									node_type;
 
+			class list_bi_iterator { //https://en.cppreference.com/w/cpp/iterator/iterator
+				public:
+					typedef std::bidirectional_iterator_tag	iterator_category;		//TODO The rest of the typdefs should come from a template arg
+
+					explicit list_bi_iterator() {}
+					//TODO
+					//constructor(node)
+					//destructor
+					//copy constructor
+					//operator=
+
+					list_bi_iterator&		operator++() {
+						link = link->next;
+					}
+
+					list_bi_iterator		operator++(int n) {		//TODO not what was meant
+						for (int i = 0; i < n; i++)
+							link = link->next;
+					}
+
+					list_bi_iterator&		operator--() {
+						link = link->prev;
+					}
+
+					list_bi_iterator		operator--(int n) {		//TODO not what was meant
+						for (int i = 0; i < n; i++)
+							link = link->prev;
+					}
+
+					bool					operator==(const list_bi_iterator& rhs) const {
+						return link == rhs.link;
+					}
+
+					bool					operator!=(const list_bi_iterator& rhs) const {
+						return link != rhs.link;
+					}
+
+					pointer					operator->() const {
+						return &link->data;
+					}
+
+					reference 				operator*() const {
+						return link->data;
+					}
+
+					node_type	*link;
+			};
+
+		public:
+			/*member type				definition notes
+			 *iterator					a bidirectional iterator to value_type	
+			 *const_iterator			a bidirectional iterator to const value_type	
+			 *reverse_iterator			reverse_iterator<iterator>	
+			 *const_reverse_iterator	reverse_iterator<const_iterator>	
+			 *node						class used for nodes	*/
+
+			//TODO
+			typedef	list_bi_iterator		iterator;
+			typedef	std::bidirectional_iterator_tag<const>	const_iterator;
+			//typedef	std::reverse_iterator<iterator>			reverse_iterator;
+			//typedef	std::reverse_iterator<const_iterator>	const_reverse_iterator;
+			//TODO make this used everywhere
+
+		private:
 			size_type					_size;
 			Alloc						alloc;
 			link*						head;
 			link*						tail;
 
 		public:
-
 			/*	(1) empty container constructor (default constructor)
 			 *		Constructs an empty container, with no elements.
 			 *	(2) fill constructor
@@ -120,27 +164,25 @@ namespace ft {
 			 *		The copy constructor (4) creates a container that keeps and 
 			 *		uses a copy of x's allocator.
 			 *
-			 *		The storage for the elements is allocated using this internal allocator.
-			 *
-			 *	https://cplusplus.com/reference/list/list/list/
-			*/
+			 *		The storage for the elements is allocated using this internal allocator.  */
 
 			explicit list (const allocator_type& alloc = allocator_type())
 								: _size(0), alloc(alloc),  head(NULL), tail(NULL) {}
 
 			explicit list (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
-								: _size(n), alloc(alloc), head(NULL), tail(NULL) {
-				for (size_type i = 0; i != _size; i++) {
+								: _size(0), alloc(alloc), head(NULL), tail(NULL) {
+				for (size_type i = 0; i != n; i++) {
 					push_back(val);
 				}
 			}
 
 			template <class InputIterator>
 			list (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
-			  					: alloc(alloc) {
+			  					: alloc(alloc), _size(0) {
 				while (first != last) {
 					push_back(*first);
 					first++;
+					_size++;
 				}
 			}
 
@@ -150,62 +192,114 @@ namespace ft {
 			}
 
 			/*	This destroys all container elements, and deallocates all the
-			 *	storage capacity allocated by the list container using its allocator.
-			 *
-			 *	https://cplusplus.com/reference/list/list/~list/
-			*/
+			 *	storage capacity allocated by the list container using its allocator.  */
 
 			~list(void) {
 				//TODO
 				//destruct stuff
 				//delete all links
+				while (!empty()) {
+					pop_back();
+					std::cout << "empty:" << empty();
+					std::cout << " size:" << size();
+					if (!empty())
+						std::cout << " tail:" << back();
+					std::cout << std::endl;
+				}
 			}
 
 			/*	Copies all the elements from x into the container.
 			 *	The container preserves its current allocator, which is used to
 			 *	allocate additional storage if needed.
 			 *	Any elements held in the container before the call are either
-			 *	assigned to or destroyed.
-			 *
-			 *	https://cplusplus.com/reference/list/list/operator=/
-			*/
+			 *	assigned to or destroyed.  */
 
 			list&				operator=(const list& x) {
 				(void)x;
 				//TODO
 			}
 
-			/*		ELEMENT ACCESS
-			*/
+			/*		CAPACITY 		*/
 
-			/*	Returns a reference to the first element in the list container.
-			*/
+			/*	Returns whether the list container is empty (i.e. whether its size is 0).  */
+
+			bool				empty() const {
+				return !_size;
+			}
+
+			/*	Returns the number of elements in the list container.  */
+
+			size_type			size() const {
+				return _size;
+			}
+
+			/* Returns the maximum number of elements that the list container can hold.  */
+
+			size_type			max_size() const {
+				return alloc.max_size();
+			}
+
+			/*		ELEMENT ACCESS 		*/
+
+			/*	Returns a reference to the first element in the list container.  */
 
 			reference 			front() {
-				return head->data;
+				return *head->data;
 			}
 			const_reference 	front() const {
-				return head->data;
+				return *head->data;
 			}
 
-			/*	Returns a reference to the last element in the list container.
-			*/
+			/*	Returns a reference to the last element in the list container.  */
 
 			reference 			back() {
-				return tail->data;
+				return *tail->data;
 			}
 			const_reference 	back() const {
-				return tail->data;
+				return *tail->data;
 			}
 
-			/*		MODIFIERS
-			*/
+			/*		MODIFIERS 			*/
+
+			/*	Inserts a new element at the beginning of the list, right before its current first element. 
+			 *	The content of val is copied (or moved) to the inserted element.
+			 *	This effectively increases the container size by one. */
+
+			void				push_front(const value_type& val) {
+				link		*l;
+
+				if (head) {
+					l = new_link(val, head, NULL);
+					head->prev = l;
+					head = l;
+				} else {
+					head = new_link(val, NULL, NULL);
+					tail = head;
+				}
+				_size++;
+			}
+
+			/*	Removes the first element in the list container, effectively reducing its size by one.
+			 *	This destroys the removed element. */
+
+			void				pop_front(void) {
+				link		*tmp;
+
+				if (head) {
+					tmp = head;
+					head = head->next;
+					if (head)
+						head->prev = NULL;
+					_size--;
+					alloc.destroy(tmp->data);
+					alloc.deallocate(tmp->data, 1);
+					delete tmp;
+				}
+			}
 
 			/*	Adds a new element at the end of the list container, after its current last element. 
-			 *	The content of val is copied (or moved) to the new element.
-			 *
-			 *	This effectively increases the container size by one.
-			*/
+			 *	The content of val is copied (or moved) to the new element.  
+			 *	This effectively increases the container size by one.  */
 
 			void				push_back(const value_type& val) {
 				if (tail) {
@@ -214,6 +308,25 @@ namespace ft {
 				} else {
 					head = new_link(val, NULL, NULL);
 					tail = head;
+				}
+				_size++;
+			}
+
+			/* Removes the last element in the list container, effectively reducing the container size by one.
+			 * This destroys the removed element.  */
+
+			void				pop_back() {
+				link		*tmp;
+
+				if (tail) {
+					tmp = tail;
+					tail = tail->prev;
+					if (tail)
+						tail->next = NULL;
+					_size--;
+					alloc.destroy(tmp->data);
+					alloc.deallocate(tmp->data, 1);
+					delete tmp;
 				}
 			}
 
@@ -225,7 +338,12 @@ namespace ft {
 				//l = alloc.allocate(sizeof(link));
 				//alloc.construct(l, val, next, prev);
 				//return l;
-				return new link(val, next, prev);
+				value_type	*tmp;
+
+				tmp = alloc.allocate(1);
+				alloc.construct(tmp, val);
+				return new link(tmp, next, prev);
+				//return new link(val, next, prev);
 			}
 	};
 
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/17 13:09:54 by averheij      #+#    #+#                 */
-/*   Updated: 2021/02/17 14:31:01 by averheij      ########   odam.nl         */
+/*   Updated: 2021/02/17 16:07:23 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,37 @@ int		main(void) {
 	ft::list<int>			*l = new ft::list<int>();
 	ft::list<int>			*l2 = new ft::list<int>(static_cast<size_t>(5), 5);
 
+	std::cout << "l" << std::endl;
 	l->push_back(1);
-	std::cout << l->front() << std::endl;
-	std::cout << l->back() << std::endl;
+	std::cout << "front " << l->front() << std::endl;
+	std::cout << "back " << l->back() << " size " << l->size() << std::endl;
 	l->push_back(2);
-	std::cout << l->back() << std::endl;
+	std::cout << "back " << l->back() << " size " << l->size() << std::endl;
 	l->push_back(3);
-	std::cout << l->back() << std::endl;
+	std::cout << "back " << l->back() << " size " << l->size() << std::endl;
 	l->push_back(4);
-	std::cout << l->back() << std::endl;
-	std::cout << l2->front() << std::endl;
-	std::cout << l2->back() << std::endl;
+	std::cout << "back " << l->back() << " size " << l->size() << std::endl;
+	l->pop_back();
+	std::cout << "back " << l->back() << " size " << l->size() << std::endl;
+	l->pop_back();
+	std::cout << "back " << l->back() << " size " << l->size() << std::endl;
+	l->pop_back();
+	std::cout << "back " << l->back() << " size " << l->size() << std::endl;
+	l->pop_back();
 
+	std::cout << "l2" << std::endl;
+	std::cout << "back " << l2->back() << " size " << l2->size() << std::endl;
+	std::cout << "back " << l2->back() << " size " << l2->size() << std::endl;
+	l2->push_front(10);
+	std::cout << "front " << l2->front() << " size " << l2->size() << std::endl;
+	l2->push_front(20);
+	std::cout << "front " << l2->front() << " size " << l2->size() << std::endl;
+	l2->pop_front();
+	std::cout << "front " << l2->front() << " size " << l2->size() << std::endl;
+
+	std::cout << "delete l" << std::endl;
 	delete l;
+	std::cout << "delete l2" << std::endl;
 	delete l2;
 	return 0;
 }
